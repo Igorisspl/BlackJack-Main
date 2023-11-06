d7179941b75fb54e6593b34085de365c017ab329
Igorisspl <S00238949@atu.ie>
2023-11-06 11:35:14 +0000

    added basic functionality

--- BlackJack Main/Program.cs
+++ BlackJack Main/Program.cs
@@ -12,28 +12,62 @@ namespace BlackJack_Main
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int player = rnd.Next(1, 22);
-            int dealer = rnd.Next(1, 22);
 
-            Console.WriteLine("player " + player);
-            Console.WriteLine("dealer " + dealer);
+            bool playAgain = true;
+            while (playAgain)
+            {
 
-            if (player > dealer)
-            {
-                Console.WriteLine("player wins");
+                int player1 = rnd.Next(1, 22);
+                int player2 = rnd.Next(1, 22);
+                int dealer1 = rnd.Next(1, 22);
+                int dealer2 = rnd.Next(1, 22);
+                int total = player1 + player2;
+                int total1 = dealer1 + dealer2;
+
+
+                Console.WriteLine(" Players Card dealt is " + player1);
+                Console.WriteLine(" Players Card dealt is " + player2);
+                Console.WriteLine("total " + total);
+
+                Console.WriteLine(" Dealers Card dealt is " + dealer1);
+                Console.WriteLine(" Dealers Card dealt is " + dealer2);
+                Console.WriteLine("total " + total1);
+
+                if (total > 21)
+                {
+                    Console.WriteLine("U lose");
+                }
+                else if (total1 > 21)
+                {
+                    Console.WriteLine("dealer lose");
+                }
+                else if (total > total1)
+                {
+                    Console.WriteLine("player wins");
+                }
+                else if (total1 > total)
+                {
+                    Console.WriteLine("dealer wins");
+                }
+                else
+                {
+                    Console.WriteLine("Its a tie");
+                }
+                Console.Write("Do you want to play again? (y/n): ");
+                string input = Console.ReadLine();
+                if (input.ToLower() != "y")
+                {
+                    playAgain = false;
+                }
             }
-            else if (dealer > player)
-            {
-                Console.WriteLine("dealer wins");
-            }
-            else
-            { 
-                Console.WriteLine("Its a tie");
-            }
-            
+
+
+
+
+
         }
-       
 
-        
+
+
     }
 }
