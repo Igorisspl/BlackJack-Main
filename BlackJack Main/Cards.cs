91297e440a0e17e6e8da9481afdffa63b8d72af9
Igorisspl <S00238949@atu.ie>
2023-11-06 10:47:54 +0000

    basic start

---
+++ BlackJack Main/Cards.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack_Main
+{
+    public enum Suit
+    {
+        Heart,
+        Diamond,
+        Ace,
+        Spade
+
+    }
+
+    public enum Face
+    {
+        King,
+        Queen,
+        Jack,
+        Ace,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten
+    }
+    internal class Cards
+    {
+        public Suit suit { get; set; }
+        public Face face { get; set; }
+         public int  Value { get; set; }
+
+         
+
+
+        
+    }
+}
--- BlackJack Main/Program.cs
+++ BlackJack Main/Program.cs
@@ -8,8 +8,32 @@ namespace BlackJack_Main
 {
     internal class Program
     {
+        private static Cards card = new Cards();
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int player = rnd.Next(1, 22);
+            int dealer = rnd.Next(1, 22);
+
+            Console.WriteLine("player " + player);
+            Console.WriteLine("dealer " + dealer);
+
+            if (player > dealer)
+            {
+                Console.WriteLine("player wins");
+            }
+            else if (dealer > player)
+            {
+                Console.WriteLine("dealer wins");
+            }
+            else
+            { 
+                Console.WriteLine("Its a tie");
+            }
+            
         }
+       
+
+        
     }
 }
