f2f46b9d3f2ce8c4659599aca02d8472894bf436
Igorisspl <S00238949@atu.ie>
2023-11-07 21:29:49 +0000

    added a play again option

--- BlackJack Main/Program.cs
+++ BlackJack Main/Program.cs
@@ -9,142 +9,114 @@ namespace BlackJack_Main
     internal class Program
     {
 
-        public static int total ;
+        public static int total;
         public static int dealerTotal;
         static void Main(string[] args)
         {
+
             //get 2 player cards
             Random rnd = new Random();
-            int player1 = rnd.Next(1, 12);
-            int player2 = rnd.Next(1, 12);
-            total = player1 + player2;
-            Console.WriteLine("Players Card dealt is: " + player1);
-            Console.WriteLine("");
-            Console.WriteLine("Players Card dealt is: " + player2);
-            Console.WriteLine("");
-            Console.WriteLine("Total: " + total);
-            Console.WriteLine("");
-            Check();
-            //if (total == 21)
-            //{
-            //    Console.WriteLine("You win");
-            //    Console.ReadLine();
-            //}
 
-
-            //repeat
             bool playAgain = true;
-            Console.Write("Do you want another card? (y/n): ");
-            string input = Console.ReadLine();
-            if (input.ToLower() != "y")
-            {
-                playAgain = false;
-            }
 
             while (playAgain)
             {
-                int player3 = rnd.Next(1, 12);
-                Console.WriteLine("Players Card dealt is " + player3);
-                total += player3;
-                Console.WriteLine("Total: " + total);
-                Check();
-                //if (total == 21)
-                //{
-                //    Console.WriteLine("You win");
-                //    Console.ReadLine();
-                //}
 
+                int player1 = rnd.Next(1, 12);
+                int player2 = rnd.Next(1, 12);
+                total = player1 + player2;
+                Console.WriteLine("Players Card dealt is: " + player1);
+                Console.WriteLine("");
+                Console.WriteLine("Players Card dealt is: " + player2);
+                Console.WriteLine("");
+                Console.WriteLine("Total: " + total);
+                Console.WriteLine("");
+                Check();
+
+                //repeat
+                bool anotherCard = true;
                 Console.Write("Do you want another card? (y/n): ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() != "y")
+                {
+                    anotherCard = false;
+                }
+
+                while (anotherCard)
+                {
+                    int player3 = rnd.Next(1, 12);
+                    Console.WriteLine("Players Card dealt is " + player3);
+                    total += player3;
+                    Console.WriteLine("Total: " + total);
+                    Check();
+
+
+                    Console.Write("Do you want another card? (y/n): ");
+                    input = Console.ReadLine();
+
+                    if (input.ToLower() != "y")
+                    {
+                        anotherCard = false;
+                    }
+                }
+
+                //dealer section
+                int dealer1 = rnd.Next(1, 12);
+                int dealer2 = rnd.Next(1, 12);
+                dealerTotal = dealer1 + dealer2;
+                Console.WriteLine("Dealers card dealt is " + dealer1);
+                Console.WriteLine("");
+                Console.WriteLine("Dealers card dealt is " + dealer2);
+                Console.WriteLine("");
+                Console.WriteLine("Dealers total: " + dealerTotal);
+                Console.WriteLine("");
+                Check();
+
+                while (dealerTotal <= 17)
+                {
+                    int dealer3 = rnd.Next(1, 12);
+                    Console.WriteLine("Dealers Card dealt is " + dealer3);
+                    dealerTotal += dealer3;
+                    Console.WriteLine("Total: " + dealerTotal);
+
+                }
+                Check();
+                Console.Write("Do you want another go? (y/n): ");
                 input = Console.ReadLine();
+
                 if (input.ToLower() != "y")
                 {
                     playAgain = false;
                 }
-
-
             }
 
 
-            //dealer section
-            int dealer1 = rnd.Next(1, 12);
-            int dealer2 = rnd.Next(1, 12);
-            dealerTotal = dealer1 + dealer2;
-            Console.WriteLine("Dealers card dealt is " + dealer1);
-            Console.WriteLine("");
-            Console.WriteLine("Dealers card dealt is " + dealer2);
-            Console.WriteLine("");
-            Console.WriteLine("Dealers total: " + dealerTotal);
-            Console.WriteLine("");
-            Check();
-            //if (dealerTotal == 21)
-            //{
-            //    Console.WriteLine("Dealer wins");
-            //    Console.ReadLine();
-            //}
-
-
-
-            while (dealerTotal <= 17)
-            {
-                int dealer3 = rnd.Next(1, 12);
-                Console.WriteLine("Dealers Card dealt is " + dealer3);
-                dealerTotal += dealer3;
-                Console.WriteLine("Total: " + dealerTotal);
-                
-                
-            }
-            Check();
-
-            ////compare results
-
-            //if (dealerTotal == 21)
-            //{
-            //    Console.WriteLine("Dealer wins");
-            //    Console.ReadLine();
-            //}
-            //if (dealerTotal > 21)
-            //{
-            //    Console.WriteLine("Dealer busts");
-            //}
-            //else if (total > 21)
-            //{
-            //    Console.WriteLine("You bust");
-            //}
-            //else if (dealerTotal > total)
-            //{
-            //    Console.WriteLine("Dealer wins");
-            //}
-            //else if (total > dealerTotal)
-            //{
-            //    Console.WriteLine("You win");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Its a tie");
-            //}
-
+           
 
         }
         static void Check()
         {
-            
+            ////compare results
             if (total == 21)
             {
                 Console.WriteLine("You win");
-                Console.ReadLine();
+
             }
-            if (dealerTotal == 21)
+            else if (dealerTotal == 21)
             {
                 Console.WriteLine("Dealer wins");
-                Console.ReadLine();
+
             }
-            if (dealerTotal > 21)
+            else if (dealerTotal > 21)
             {
                 Console.WriteLine("Dealer busts");
+                return;
             }
             else if (total > 21)
             {
                 Console.WriteLine("You bust");
+                return;
             }
             else if (dealerTotal > total)
             {
