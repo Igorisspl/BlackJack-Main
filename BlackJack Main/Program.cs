7c351c286457f3340d9d5d7b0d6e3f0c148cbb14
Igorisspl <S00238949@atu.ie>
2023-11-08 10:39:31 +0000

    fixed player turn not displaying properly +

--- BlackJack Main/Program.cs
+++ BlackJack Main/Program.cs
@@ -15,76 +15,25 @@ namespace BlackJack_Main
         public static bool playAgain;
         static void Main(string[] args)
         {
-
-            //get 2 player cards
+          
             Random rnd = new Random();
 
             playAgain = true;
 
             while (playAgain)
             {
-
-                //int player1 = rnd.Next(1, 12);
-                //int player2 = rnd.Next(1, 12);
-                //total = player1 + player2;
-                //Console.WriteLine("Players Card dealt is: " + player1);
-                //Console.WriteLine("");
-                //Console.WriteLine("Players Card dealt is: " + player2);
-                //Console.WriteLine("");
-                //Console.WriteLine("Total: " + total);
-                //Console.WriteLine("");
+                //get 2 player cards
                 PlayerTurn();
                 Check();
 
-                //repeat
-                bool anotherCard = true;
-                Console.Write("Do you want another card? (y/n): ");
-                input = Console.ReadLine();
-
-                if (input.ToLower() != "y")
-                {
-                    anotherCard = false;
-                }
-
-                while (anotherCard)
-                {
-                    int player3 = rnd.Next(1, 12);
-                    Console.WriteLine("Players Card dealt is " + player3);
-                    total += player3;
-                    Console.WriteLine("Total: " + total);
-                    Check();
-
-                    Console.Write("Do you want another card? (y/n): ");
-                    input = Console.ReadLine();
-
-                    if (input.ToLower() != "y")
-                    {
-                        anotherCard = false;
-                    }
-                }
+                //dealer section
                 Dealer();
                 Check();   
-                //dealer section
-                //int dealer1 = rnd.Next(1, 12);
-                //int dealer2 = rnd.Next(1, 12);
-                //dealerTotal = dealer1 + dealer2;
-                //Console.WriteLine("Dealers card dealt is " + dealer1);
-                //Console.WriteLine("");
-                //Console.WriteLine("Dealers card dealt is " + dealer2);
-                //Console.WriteLine("");
-                //Console.WriteLine("");
-                //Console.WriteLine("Dealers total: " + dealerTotal);
-                //Check();
-
-                //while (dealerTotal <= 17)
-                //{
-                //    int dealer3 = rnd.Next(1, 12);
-                //    Console.WriteLine("Dealers Card dealt is " + dealer3);
-                //    dealerTotal += dealer3;
-                //    Console.WriteLine("Total: " + dealerTotal);
-                //    Check();
-                //}
+                  
+                //check 
                 CheckResults();
+
+                //repeat
                 Console.Write("Do you want another go? (y/n): ");
                 input = Console.ReadLine();
 
@@ -99,7 +48,7 @@ namespace BlackJack_Main
         {
             Random rnd1 = new Random();
 
-            while (total < 21)
+            if (total < 21)
             {
                 int playerCard = rnd1.Next(1, 12);
                 Console.WriteLine("Players Card dealt is " + playerCard);
@@ -108,14 +57,33 @@ namespace BlackJack_Main
                 total = playerCard + playerCard2;
                 Console.WriteLine("Total: " + total);
 
+                bool anotherCard = true;
+
                 Console.Write("Do you want another card? (y/n): ");
                 input = Console.ReadLine();
 
                 if (input.ToLower() != "y")
                 {
-                    break;
+                    anotherCard = false;
+                }
+
+                while (anotherCard)
+                {
+                    int player3 = rnd1.Next(1, 12);
+                    Console.WriteLine("Players Card dealt is " + player3);
+                    total += player3;
+                    Console.WriteLine("Total: " + total);
+
+                    Console.Write("Do you want another card? (y/n): ");
+                    input = Console.ReadLine();
+
+                    if (input.ToLower() != "y")
+                    {
+                        anotherCard = false;
+                    }
                 }
             }
+            
         }
         static void Dealer()
         {
@@ -142,28 +110,14 @@ namespace BlackJack_Main
         }
         static void Check()
         {
-            ////compare results
+            ////check results
             if (total == 21)
             {
-                Console.WriteLine("BlackJack!");
-                //Console.Write("Do you want another go? (y/n): ");
-                //input = Console.ReadLine();
-
-                //if (input.ToLower() != "y")
-                //{
-                //    playAgain = false;
-                //}
+                Console.WriteLine("BlackJack!");            
             }
             else if (dealerTotal == 21)
             {
-                Console.WriteLine("BlackJack! (Dealer)");
-                //Console.Write("Do you want another go? (y/n): ");
-                //input = Console.ReadLine();
-
-                //if (input.ToLower() != "y")
-                //{
-                //    playAgain = false;
-                //}
+                Console.WriteLine("BlackJack! (Dealer)");              
             }
             else if (dealerTotal > 21)
             {
@@ -178,6 +132,7 @@ namespace BlackJack_Main
                 Console.WriteLine("");
             }
         }
+        //compare
         static void CheckResults()
         {
             if (dealerTotal > total)
@@ -200,3 +155,59 @@ namespace BlackJack_Main
         //}
     }
 }
+//dealer section
+//int dealer1 = rnd.Next(1, 12);
+//int dealer2 = rnd.Next(1, 12);
+//dealerTotal = dealer1 + dealer2;
+//Console.WriteLine("Dealers card dealt is " + dealer1);
+//Console.WriteLine("");
+//Console.WriteLine("Dealers card dealt is " + dealer2);
+//Console.WriteLine("");
+//Console.WriteLine("");
+//Console.WriteLine("Dealers total: " + dealerTotal);
+//Check();
+
+//while (dealerTotal <= 17)
+//{
+//    int dealer3 = rnd.Next(1, 12);
+//    Console.WriteLine("Dealers Card dealt is " + dealer3);
+//    dealerTotal += dealer3;
+//    Console.WriteLine("Total: " + dealerTotal);
+//    Check();
+//}
+
+//int player1 = rnd.Next(1, 12);
+//int player2 = rnd.Next(1, 12);
+//total = player1 + player2;
+//Console.WriteLine("Players Card dealt is: " + player1);
+//Console.WriteLine("");
+//Console.WriteLine("Players Card dealt is: " + player2);
+//Console.WriteLine("");
+//Console.WriteLine("Total: " + total);
+//Console.WriteLine("");
+
+//bool anotherCard = true;
+//Console.Write("Do you want another card? (y/n): ");
+//input = Console.ReadLine();
+
+//if (input.ToLower() != "y")
+//{
+//    anotherCard = false;
+//}
+
+//while (anotherCard)
+//{
+//    int player3 = rnd.Next(1, 12);
+//    Console.WriteLine("Players Card dealt is " + player3);
+//    total += player3;
+//    Console.WriteLine("Total: " + total);
+//    Check();
+
+//    Console.Write("Do you want another card? (y/n): ");
+//    input = Console.ReadLine();
+
+//    if (input.ToLower() != "y")
+//    {
+//        anotherCard = false;
+//    }
+//}
