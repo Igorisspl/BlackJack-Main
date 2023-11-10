759927a98a6a84f9484759ef8b3d5555fe82a4d8
Igorisspl <S00238949@atu.ie>
2023-11-10 15:40:40 +0000

    Final code, added comments formatting

--- BlackJack Main/Cards.cs
+++ BlackJack Main/Cards.cs
@@ -6,6 +6,27 @@ using System.Threading.Tasks;
 
 namespace BlackJack_Main
 {
-           
-    
+    // Define a class representing a playing card
+    public class Card
+    {
+        // Properties for the card's suit, rank, and value
+        public string Suit { get; }
+        public string Rank { get; }
+        public int Value { get; }
+
+        // Constructor to initialize the card with suit, rank, and value
+        public Card(string suit, string rank, int value)
+        {
+            Suit = suit;
+            Rank = rank;
+            Value = value;
+        }
+
+        // Override the ToString() method to provide a string representation of the card
+        public override string ToString()
+        {
+            // Format the card as "Rank of Suit"
+            return $"{Rank} of {Suit}";
+        }
+    }
 }
--- BlackJack Main/Program.cs
+++ BlackJack Main/Program.cs
@@ -8,105 +8,162 @@ namespace BlackJack_Main
 {
     internal class Program
     {
-
+        // Declare variables for game state
         public static int total;
         public static int dealerTotal;
         public static string input;
         public static bool playAgain;
+
         static void Main(string[] args)
         {
-
-            Random rnd = new Random();
-
+            // Set playAgain to true to enter the game loop
             playAgain = true;
 
+            // Game loop
             while (playAgain)
             {
+                // Play a round of the game
                 PlayGame();
+
+                // Ask if the player wants to play again
                 Console.Write("Do you want another go? (y/n): ");
                 input = Console.ReadLine();
 
+                // If the input is not 'y', set playAgain to false to exit the loop
                 if (input.ToLower() != "y")
                 {
                     playAgain = false;
                 }
             }
-
         }
+
+        // Method to play a round of the game
         static void PlayGame()
         {
-            PlayerTurn();
+            // Initialize and shuffle the deck
+            List<Card> deck = InitializeDeck();
+            ShuffleDeck(deck);
 
-            Dealer();
+            // Player's turn
+            PlayerTurn(deck);
 
+            // Dealer's turn
+            Dealer(deck);
+
+            // Compare results
             CheckResults();
         }
-        static void PlayerTurn()
+
+        // Method to initialize a deck of cards
+        static List<Card> InitializeDeck()
         {
-            Random rnd1 = new Random();
-            total = 0;
-            while (total < 21)
+            List<Card> deck = new List<Card>();
+
+            // Define suits and ranks for the cards
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+            // Create cards and add them to the deck
+            foreach (var suit in suits)
             {
-                int player1 = rnd1.Next(1, 12);
-                Console.WriteLine("Players Card dealt is " + player1);
-                int player2 = rnd1.Next(1, 12);
-                Console.WriteLine("Players Card dealt is " + player2);
-                total = player1 + player2;
-                Console.WriteLine("Total: " + total);
-
-                bool anotherCard = true;
-
-                Console.Write("Do you want another card? (y/n): ");
-                input = Console.ReadLine();
-
-                if (input.ToLower() != "y")
+                foreach (var rank in ranks)
                 {
-                    anotherCard = false;
-                }
-
-                while (anotherCard)
-                {
-                    int player3 = rnd1.Next(1, 12);
-                    Console.WriteLine("Player takes another card " + player3);
-                    total += player3;
-                    Console.WriteLine("Total: " + total);
-
-                    Console.Write("Do you want another card? (y/n): ");
-                    input = Console.ReadLine();
-
-                    if (input.ToLower() != "y")
+                    int value;
+                    if (int.TryParse(rank, out value))
                     {
-                        anotherCard = false;
+                        deck.Add(new Card(suit, rank, value));
+                    }
+                    else
+                    {
+                        deck.Add(new Card(suit, rank, 10));
                     }
                 }
             }
-
+            return deck;
         }
-        static void Dealer()
+
+        // Method to shuffle the deck using the Fisher-Yates algorithm
+        static void ShuffleDeck(List<Card> deck)
         {
             Random rnd = new Random();
-            int dealer1 = rnd.Next(1, 12);
-            int dealer2 = rnd.Next(1, 12);
-            dealerTotal = dealer1 + dealer2;
-            Console.WriteLine("Dealers card dealt is " + dealer1);
-            Console.WriteLine("");
-            Console.WriteLine("Dealers card dealt is " + dealer2);
-            Console.WriteLine("");
-
-            while (dealerTotal <= 17)
+            int n = deck.Count;
+            while (n > 1)
             {
-                int dealer3 = rnd.Next(1, 12);
-                Console.WriteLine("Dealer takes another card " + dealer3);
-                dealerTotal += dealer3;
-
+                n--;
+                int k = rnd.Next(n + 1);
+                Card value = deck[k];
+                deck[k] = deck[n];
+                deck[n] = value;
             }
         }
 
-        //compare
+        // Method for the player's turn
+        static void PlayerTurn(List<Card> deck)
+        {
+            total = 0;
+
+            Console.WriteLine("Player's turn:");
+
+            // Allow the player to draw cards until they choose to stop
+            while (total <= 21)
+            {
+                Card drawnCard = DrawCard(deck);
+                Console.WriteLine("Players Card dealt is " + drawnCard);                 //only 1 inital card given due to some bug
+                total += drawnCard.Value;
+                Console.WriteLine("Total: " + total);
+
+                // Ask if the player wants another card
+                Console.Write("Do you want another card? (y/n): ");
+                input = Console.ReadLine();
+
+                // If the input is not 'y', exit the loop
+                if (input.ToLower() != "y")
+                {
+                    break;
+                }
+            }
+        }
+
+        // Method for the dealer's turn
+        static void Dealer(List<Card> deck)
+        {
+            dealerTotal = 0;
+
+            Console.WriteLine("\nDealer's turn:");
+
+            // Dealer draws cards until their total is 17 or higher
+            while (dealerTotal <= 17)
+            {
+                Card drawnCard = DrawCard(deck);
+                Console.WriteLine("Dealer takes another card " + drawnCard);
+                dealerTotal += drawnCard.Value;
+            }
+        }
+
+        // Method to draw a card from the deck
+        static Card DrawCard(List<Card> deck)
+        {
+            // Check if the deck is empty and reshuffle if needed
+            if (deck.Count == 0)
+            {
+                Console.WriteLine("Out of cards. Reshuffling deck.");
+                deck = InitializeDeck();
+                ShuffleDeck(deck);
+            }
+
+            // Draw the top card from the deck
+            Card drawnCard = deck[0];
+            deck.RemoveAt(0);
+            return drawnCard;
+        }
+
+        // Method to compare results and determine the winner
         static void CheckResults()
         {
-            Console.WriteLine("Your total: " + total);
+            Console.WriteLine("\nYour total: " + total);
             Console.WriteLine("Dealer's total: " + dealerTotal);
+
+            // Compare total values to determine the winner or a tie
             if (total == 21)
             {
                 Console.WriteLine("BlackJack!");
@@ -133,11 +190,9 @@ namespace BlackJack_Main
             }
             else if (total == dealerTotal)
             {
-                Console.WriteLine("Its a tie");
+                Console.WriteLine("It's a tie");
             }
             else { Console.WriteLine(""); }
-
         }
-
     }
 }
